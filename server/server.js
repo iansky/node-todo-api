0cae17f583e691a6db3f3429b20090e0c4c55541
iansky <njorogeian20@gmail.com>
2017-05-18 19:42:02 +0300

    private route with creator

--- server/server.js
+++ server/server.js
@@ -15,9 +15,10 @@ var app = express();
 var port = process.env.PORT || 3000;
 app.use(bodyParser.json())
 
-app.post('/todos',(req,res)=>{
+app.post('/todos',authenticate,(req,res)=>{
   var todo = new Todo({
-      text:req.body.text
+      text:req.body.text,
+      _creator:req.user._id
   });
 
   todo.save().then((docs)=>{
@@ -27,15 +28,17 @@ app.post('/todos',(req,res)=>{
   });
 });
 
-app.get('/todos',(req,res)=>{
-  Todo.find().then((doc)=>{
+app.get('/todos',authenticate,(req,res)=>{
+  Todo.find({
+      _creator:req.user._id
+  }).then((doc)=>{
      res.send({doc})
   },(err)=>{
       res.status(400).send(err);
   });
 });
 
-app.get('/todos/:id',(req,res)=>{
+app.get('/todos/:id',authenticate,(req,res)=>{
    
    var id =  req.params.id;
 
@@ -43,7 +46,11 @@ app.get('/todos/:id',(req,res)=>{
        res.status(400).send();
    }
 
-   Todo.findById(id).then((todo)=>{
+   Todo.findById({
+       id:_id,
+       _creator:req.user._id
+
+   }).then((todo)=>{
        if(!todo){
            res.status(400).send();
        }
@@ -53,14 +60,17 @@ app.get('/todos/:id',(req,res)=>{
    });
 });
 
-app.delete('/todos/:id',(req,res)=>{
+app.delete('/todos/:id',authenticate,(req,res)=>{
     var id = req.params.id;
 
     if(!ObjectID.isValid(id)){
         res.status(400).send();
     }
      
-    Todo.findByIdAndRemove(id).then((todo)=>{
+    Todo.findOneAndRemove({
+         _id:id,
+         _creator:req.user._id
+    }).then((todo)=>{
           if(!todo){
            res.status(400).send();
        }
@@ -70,7 +80,7 @@ app.delete('/todos/:id',(req,res)=>{
     });
 });
 
-app.patch('/todos/:id',(req,res)=>{
+app.patch('/todos/:id',authenticate,(req,res)=>{
     var id  = req.params.id;
 
     var body = _.pick(req.body,['text','completed']);
@@ -88,7 +98,7 @@ app.patch('/todos/:id',(req,res)=>{
         body.completedAt = null;
     }
 
-    Todo.findByIdAndUpdate(id,{$set : body},{new : true}).then((todo)=>{
+    Todo.findOneAndUpdate({_id:id,_creator:req.user._id},{$set : body},{new : true}).then((todo)=>{
         if(!todo){
         return res.status(400).send();
     }
@@ -133,6 +143,15 @@ app.post('/users/login',(req,res)=>{
         res.status(400).send();
     });
 });
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
+
 app.listen(port,()=>{
   console.log(`Listening to port ${port}`);
 });
