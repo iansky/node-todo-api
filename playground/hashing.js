c7c255deae9db939282950252ae642c65d42ad1f
iansky <njorogeian20@gmail.com>
2017-05-18 14:04:00 +0300

    hashed password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,18 +1,31 @@
 const {SHA256} = require('crypto-js');
-
 const jwt = require('jsonwebtoken');
 
-var data = {
-    id:1
-}
+const  bcrypt = require('bcryptjs');
+
+var password = '123456'
+// bcrypt.genSalt(10,(err,salt)=>{
+//     bcrypt.hash(password,salt,(err,hash)=>{
+//         console.log(hash);
+//     });
+// });
+
+var hashedPassword = '$2a$10$9JKI9vyFyAcVgzFoc5.Gae.V4LQwVgJTPo/t7lkrkYIVKX.gaacqm';
+
+bcrypt.compare(password,hashedPassword,(err,res)=>{
+  console.log(res);
+});
+// var data = {
+//     id:1
+// }
 //take the object i.e user id and sign it create hash and return hash value
-var token = jwt.sign(data,'123456');
-console.log(token);
+// var token = jwt.sign(data,'123456');
+// console.log(token);
 
 //take the hash value and make sure is not checked
 //jwt.verify;
-var decoded = jwt.verify(token,'123456');
-console.log(decoded);
+// var decoded = jwt.verify(token,'123456');
+// console.log(decoded);
 // var message = "I am user number 3";
 
 // var hash = SHA256(message).toString();
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt =require('jsonwebtoken');
 const  _ = require('lodash');
+const bycryt = require('bcryptjs');
 
 var UserSchema = mongoose.Schema({
    email:{
@@ -75,8 +76,26 @@ UserSchema.statics.findByToken = function(token){
 };
 
 
-var User = mongoose.model('User',UserSchema);
+UserSchema.pre('save',function(next){
+    var user = this;
 
+    if(user.isModified('password')){
+
+        var password = user.password;
+
+        bycryt.genSalt(10,(err,salt)=>{
+            bycryt.hash(password,salt,(err,hash)=>{
+             user.password = hash;
+               next();
+            });
+        });
+
+    }else{
+        next();
+    }
+});
+
+var User = mongoose.model('User',UserSchema);
 
 module.exports = {User};
 
